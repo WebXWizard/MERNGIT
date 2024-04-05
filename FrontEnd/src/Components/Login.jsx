b629781d50c02bb608cf9092f1ac50f7fcb34237
Shahe Alam <alamgit9044@gmail.com>
2024-04-05 08:59:14 +0530

    Login Page

--- FrontEnd/src/Components/Login.jsx
+++ FrontEnd/src/Components/Login.jsx
@@ -2,35 +2,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <>
-      <div className="wrapper">
-        <div className="main-content">
-
-          <div className="header">
-            <h5>Member Login</h5>
-          </div><br></br>
-
-          <div className="l-part">
-            <input type="text" placeholder="Username" className="input-1" />
-
-            <div className="overlap-text">
-              <input type="password" placeholder="Password" className="input-2" />
-              <br></br>
-            </div>
-
-            <input type="button" defaultValue="Log in" className="btn" />
-          </div>
-        </div>
-
-        <div className="sub-content">
-          <div className="s-part">
-            Don't have an account?<a href="#">Sign up</a>
-          </div>
-        </div>
-      </div>
-     
-    </>
-
+  <div>
+    <h3>Login Page</h3>
+  </div>
 
   )
 }
